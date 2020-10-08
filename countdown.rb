fc52cf4babd9b505ffad66023a5af00cf8481f9e
iamShah <formal>
2020-10-07 21:30:16 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
+# #write your code here
+# x=
+# while countdown>x
 
-def countdown
+# def countdown
+    
+# end
+x = 10
+while x > 10
+  puts "#{x} HAPPY NEW YEAR!"
+  x += 1
 end
