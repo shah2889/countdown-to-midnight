10d9e6d5e05303293ead12d0b079431458b6749e
iamShah <formal>
2020-11-11 19:56:16 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -19,3 +19,12 @@ def countdown(number)
 
   end
   
+  def countdown(number)
+    number =10
+    while number <=10
+      sleep 1
+      puts "#{number} SECOND(S)!"
+      number -=1
+    end 
+  end
+
